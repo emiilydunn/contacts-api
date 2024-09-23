893a901b39889c60a5f3ce95af50f70cc027861c
Emily Dunn <emilydunn13@gmail.com>
2024-09-23 10:59:08 -0300

    working on stuff!

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,57 +1,109 @@
 import express from 'express';
 import multer from 'multer';
+import { PrismaClient } from '@prisma/client';
+
 
 const router = express.Router();
 
+//Prisma setup
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
+
+
 //Multer setup
 const storage = multer.diskStorage({
-   destination: function (req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, 'public/images/'); // save uploaded files in `public/images` folder
-   },
-   filename: function (req, file, cb) {
+  },
+  filename: function (req, file, cb) {
     //Get file extension
-    const ext = file.originalname.split('.').pop(); 
+    const ext = file.originalname.split('.').pop();
     //Generate unique filename - current timestamp + random number between 0 and 1000.
-    const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext; 
+    const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext;
     cb(null, uniqueFilename);
-   }
-  });
+  }
+});
 const upload = multer({ storage: storage });
-  
-  
 
-router.get('/', (req, res) => {
-  res.send('Updated Contacts route - testing nodemon');
+
+//Get all contacts with Prisma 
+router.get('/get/all', async (req, res) => {
+  const contact = await prisma.contact.findMany()
+
+  res.json(contact);
 });
 
-//Get all contacts
-router.get('/all', (req, res) => {
-  res.send('All contacts');
-});
 
-//Get a contact by ID
-router.get('/:id', (req, res) => {
+//Get a contact by ID with Prisma
+router.get('/get/:id', async (req, res) => {
   const id = req.params.id;
-  res.send('Contact by id ' + id);
+
+  //Validate if id is a number
+  if (isNaN(id)) {
+    res.status(400).json({ message: 'Invalid ID. Please try again.' });
+    return;
+  }
+
+  const contact = await prisma.contact.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  })
+
+  //If statement to output valid/invalid ID
+  if (contact) {
+    res.json(contact);
+  } else {
+    res.status(404).json({ message: "Contact not found. Please try again." })
+  }
+
 });
+
+
 
 //Add post, put, and delete routers
 
 //Create new contact (with Multer)
 // .../api/contacts/create
-router.post('/create', upload.single('image'), (req, res) => {
-  const filename = req.file ? req.file.filename : '';
-  const { first_name, last_name, email, phone } = req.body;
+router.post('/create', upload.single('image'), async (req, res) => {
+  const filename = req.file ? req.file.filename : null;
+  const { firstName, lastName, title, email, phone } = req.body;
 
-  //const firstName = req.body.first_name;
-  //const lastName = req.body.last_name;
+  if(!firstName || !lastName || !email || !phone) {
+    return res.status(400).json({ message: 'Required fields must have a value.'});
+  }
+  //Error handling - fix this later
+  //To-do: Delete uploaded file
+  /*if (!firstName) {
+    return res.status(400).json({ message: "Please include first name." });
+  }
+  if (!lastName) {
+    return res.status(400).json({ message: "Please include last name." });
+  }
+  if (!email) {
+    return res.status(400).json({ message: "Please include email." });
+  }
+  if (!phone) {
+    return res.status(400).json({ message: "Please include phone." });
+  }*/
 
-  console.log('Uploaded file: ' + filename);
-  console.log(`My contact's name: ${first_name} ${last_name}`);
-  console.log(`My contact's phone number: ${phone}. Email address: ${email}.`)
+  //Prisma Create
+  const contact = await prisma.contact.create({
+    data: {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phone: phone,
+      title: title,
+      filename: filename,
+    },
+  });
 
-  res.send('Create a new Contact');
+  res.json(contact);
 });
+
+//Look up Prisma update and delete in documentation
 
 //Update contact by ID (with Multer)
 // .../api/contacts/update
